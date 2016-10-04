ef1d7bf983c94cfaf41f1f1d9ee2d2a41b0281a5
Ninad Bandodkar <nbandodk@github.iu.edu>
2016-10-04 19:15:57 -0400

    modified prodcons

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -11,8 +11,7 @@ shellcmd xsh_prodcons(int nargs, char *args[])
 {
 	int count = 2000;             //local varible to hold count
 	
-	produced = semcreate(0);
-	consumed = semcreate(1);
+	
 	
 	//Argument verifications and validations
 	if (nargs > 2) {
@@ -27,10 +26,39 @@ shellcmd xsh_prodcons(int nargs, char *args[])
 		return 0;
 	}
 	
-	 //check args[1] if present assign value to count
-	if (nargs == 2) {
-		
-		count = atoi(args[1]);
+	 
+	 
+	 //check args[1] if present and take appropriate action
+	if (nargs == 2) 
+	{
+	
+		if(strncmp(args[1], "-f")==0)
+		{
+			future *f1, *f2, *f3;
+			
+			f1 = future_alloc(FUTURE_EXCLUSIVE);
+  			f2 = future_alloc(FUTURE_EXCLUSIVE);
+			f3 = future_alloc(FUTURE_EXCLUSIVE);
+ 
+ 			resume( create(future_cons, 1024, 20, "fcons1", 1, f1) );
+  			resume( create(future_prod, 1024, 20, "fprod1", 1, f1) );
+  			resume( create(future_cons, 1024, 20, "fcons2", 1, f2) );
+  			resume( create(future_prod, 1024, 20, "fprod2", 1, f2) );
+  			resume( create(future_cons, 1024, 20, "fcons3", 1, f3) );
+  			resume( create(future_prod, 1024, 20, "fprod3", 1, f3) );
+		}
+		else
+		{
+			count = atoi(args[1]);
+			
+			
+      			produced = semcreate(0);
+			consumed = semcreate(1);
+			
+			//create the process producer and consumer and put them in ready queue.
+  			resume( create(produce, 1024, 20, "producer", 1, count));
+  			resume( create(consume, 1024, 20, "consumer", 1, count));
+  		}
 		
 	
 	}
@@ -41,10 +69,7 @@ shellcmd xsh_prodcons(int nargs, char *args[])
 
  
 
-  //create the process producer and consumer and put them in ready queue.
-  //Look at the definations of function create and resume in the system folder for reference.      
-  resume( create(produce, 1024, 20, "producer", 1, count));
-  resume( create(consume, 1024, 20, "consumer", 1, count));
+  
   return (0);
 
 }
